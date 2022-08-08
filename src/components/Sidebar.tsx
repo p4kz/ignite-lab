01d78d04c4ec3397c190e40133350edf07de520d
p4kz <wolpak@hotmail.com>
2022-08-08 12:45:00 -0300

    create event screen / styles / fetch

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,7 +1,6 @@
 import { gql, useQuery } from "@apollo/client";
 import { Lesson } from "./Lesson";
 
-
 const GET_LESSONS_QUERY = gql`
   query {
     lessons(orderBy: availableAt_ASC, stage: PUBLISHED) {
